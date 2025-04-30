3da1681ff7341ba2de4546ea6d1aa253d158a50b
AjayTeotia <teotiaajay20@gmail.com>
2025-04-30 19:55:49 +0530

    signup-controller

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,6 +1,61 @@
-// Sign up 
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+// Sign up
 export async function Signup(req, res) {
-  res.send("signup");
+  const { email, password, fullName } = req.body;
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const exitingUser = await User.findOne({ email });
+    if (exitingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    // TODO: CREATE NEW USER IN STREAM
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log("Error in sign up controller", error);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
 }
 
 // Login
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,21 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import express from "express";
 
-import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth", authRoute)
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    connectDB();
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
