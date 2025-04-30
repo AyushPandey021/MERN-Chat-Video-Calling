cd1c245e47a6f5dd7c67c9c1ab92eab35b1c7249
AjayTeotia <teotiaajay20@gmail.com>
2025-04-30 16:53:50 +0530

    initial-project-setup

---
+++ server/src/controllers/auth.controllers.js
@@ -0,0 +1,14 @@
+// Sign up 
+export async function Signup(req, res) {
+  res.send("signup");
+}
+
+// Login
+export async function Login(req, res) {
+  res.send("login");
+}
+
+// Logout
+export async function Logout(req, res) {
+  res.send("logout");
+}
