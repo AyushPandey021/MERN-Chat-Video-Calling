e2e74456e1eae45c556c1922c2c61e73af10557f
AjayTeotia <teotiaajay20@gmail.com>
2025-05-01 14:26:50 +0530

    get-friend-requests-and-get-outgoing-friend-requests

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -118,3 +118,42 @@ export async function acceptFriendRequest(req, res) {
     res.status(500).json({ message: "INTERNAL SERVER ERROR" });
   }
 }
+
+export async function getFriendRequests(req, res) {
+  try {
+    const incomingRequests = await FriendRequest.find({
+      recipient: req.user._id,
+      status: "pending",
+    }).populate(
+      "sender",
+      "fullName profilePic nativeLanguage learningLanguage"
+    );
+
+    const acceptedRequests = await FriendRequest.find({
+      sender: req.user._id,
+      status: "accepted",
+    }).populate("recipient", "fullName profilePic");
+
+    res.status(200).json({ incomingRequests, acceptedRequests });
+  } catch (error) {
+    console.error("Error in getting friend requests:", error.message);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
+}
+
+export async function getOutgoingFriendReqs(req, res) {
+  try {
+    const outgoingRequests = await FriendRequest.find({
+      sender: req.user._id,
+      status: "pending",
+    }).populate(
+      "recipient",
+      "fullName profilePic nativeLanguage learningLanguage"
+    );
+
+    res.status(200).json(outgoingRequests);
+  } catch (error) {
+    console.error("Error in getting outgoing friend requests:", error.message);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
+}
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,7 +2,9 @@ import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   acceptFriendRequest,
+  getFriendRequests,
   getMyFriends,
+  getOutgoingFriendReqs,
   getRecommendedUsers,
   sendFriendRequest,
 } from "../controllers/user.controllers.js";
@@ -18,6 +20,6 @@ router.get("/friends", getMyFriends);
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-
-
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 export default router;
