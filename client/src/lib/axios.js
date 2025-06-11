cd3d521084a1a3983c1244caaebdbc10bfaf95b3
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-06-11 20:28:06 +0530

    axios

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+  import.meta.env.MODE === "development" ? "https://fullstack-chat-video-gamma.vercel.app/" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
