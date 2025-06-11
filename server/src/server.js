e80327617ed916e454ca252de28cdfb1cd30c2c8
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-06-11 20:34:15 +0530

    cors-update

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fullstack-chat-video-calling-app-rg.vercel.app/login",
     credentials: true,
   })
 );
