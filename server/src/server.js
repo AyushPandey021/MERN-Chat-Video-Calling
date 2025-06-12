a61fc2c82b683170bdd7ec2024059ea563ad7fbc
ApDev. <143201999+AyushPandey021@users.noreply.github.com>
2025-06-12 20:13:49 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://fullstack-chat-video-calling-app-rg.vercel.app/login",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
