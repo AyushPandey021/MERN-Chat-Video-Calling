47714ef52693be3ec69cb7aec95523bc96417ffe
ApDev. <143201999+AyushPandey021@users.noreply.github.com>
2025-09-01 07:42:59 +0530

    FIX db.js

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connection = await mongoose.connect("mongodb://127.0.0.1:27017/livechat");
+      const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected:🎉`);
   } catch (error) {
     console.log("Error in connecting to MongoDB", error);
